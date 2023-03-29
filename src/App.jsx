72c32f0d962b7bb1163583285666f3e173c5af8f
Jeremy <[arkantik.pro@gmail.com](mailto:arkantik.pro@gmail.com)>
2023-03-29 20:15:01 +0200

    Correction of props.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import PokemonCard from './components/PokemonCard.jsx'
 import './App.css'
 
-function App() {
-
+function App(){
   
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
   return (
     <div>
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[0]}/>
     </div>
   );
 }
