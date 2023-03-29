5272d8aa28211e2da5d16d810a82e61e480cccdd
Jeremy <[arkantik.pro@gmail.com](mailto:arkantik.pro@gmail.com)>
2023-03-29 11:48:46 +0200

    Added a state to my PokemonList

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import PokemonCard from './components/PokemonCard.jsx'
 import './App.css'
 
@@ -7,18 +6,47 @@ function App(){
   
   const pokemonList = [
     {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
+        name: "bulbasaur",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+      {
+        name: "charmander",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      },
+      {
+        name: "squirtle",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      },
+      {
+        name: "pikachu",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      },
+      {
+        name: "mew",
+      },
   ];
+
+  const [pokemonIndex, setpokemonIndex] = useState(0);
+  const [showButton, setShowButton] = useState(true);
+  const [noshowButton, setNoShowButton] = useState(false);
+
+  const handleClick = () => {
+    setpokemonIndex(pokemonIndex - 1)
+  };
+
+  const handleClick1 = () => {
+    setpokemonIndex(pokemonIndex + 1)
+  };
 
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]}/>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
+      {pokemonIndex > 0 ? <button onClick={handleClick}>Previous</button> : null}
+      {pokemonIndex < pokemonList.length -1 ? <button onClick={handleClick1}>Next</button> : null}
     </div>
   );
 }
