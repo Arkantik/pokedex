1276d08d2ec4a19cfb0e1f39adae46dbc9b668f1
Jeremy Dohin <95821033+Arkantik@users.noreply.github.com>
2023-03-30 12:08:29 +0200

    Update App.jsx
    Correction of broken code

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,48 @@ function App(){
   
   const pokemonList = [
     {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
+        name: "bulbasaur",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+      {
+        name: "charmander",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      },
+      {
+        name: "squirtle",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      },
+      {
+        name: "pikachu",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      },
+      {
+        name: "mew",
+      },
   ];
+
+  const [pokemonIndex, setpokemonIndex] = useState(0);
+
+  const handleClick = () => {
+    setIndex(index - 1);
+  };
+
+  const handleClick1 = () => {
+    setIndex(index + 1);
+  };
 
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]}/>
+      <PokemonCard pokemon={pomemonList[pokemonindex]} />
+      {pokemonIndex > 0 ? (<button onClick={handleClick}>Previous</button>) : null}
+      {pokemonIndex < pokemonList.length - 1 ? (<button onClick={handleClick1}>Next</button>) : null}
     </div>
   );
-}
+};
+
 
 export default App
